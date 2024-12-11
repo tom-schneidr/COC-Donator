11b12ffd856fdd9a1428d9f2bd3bbcc79f13f5af
Tom Schneider <tom.laurin.schneider@gmail.com>
2024-12-11 11:30:20 +0100

    Added fundamental discord connection logic

---
+++ COCDonator/Discord/DiscordCommands.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COCDonator.Discord
+{
+  public class DiscordCommands : InteractionModuleBase<SocketInteractionContext>
+  {
+    // Constructor to accept MainWindow instance
+    public DiscordCommands(MainWindow mainWindow)
+    {
+    }
+
+    [SlashCommand("addblacklistedplayer", "Add a player to the blacklist")]
+    public async Task AddPlayerToBlacklist(string playerTag)
+    {
+      await RespondAsync("Player: " + playerTag + " is now blacklisted and wont be invited anymore.");
+    }
+  }
+}
